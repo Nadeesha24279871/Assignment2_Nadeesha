0b7e331e8d0dd1c15133db3271de8ae8494bed94
Nadeesha24279871 <n.adasuriya.mudiyanselage.10@student.scu.edu.au>
2023-09-29 10:02:05 +1000

    creating the menu
    Using switch to create the menu

--- Assignment2/studentStatistics.java
+++ Assignment2/studentStatistics.java
@@ -1,22 +1,56 @@
 
 /**
- * Write a description of class studentStatistics here.
+ *  This program is developed to compute statistics of students' marks in an assignment.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Nadeesha)
+ * @version (24/09/2023)
  */
+import java.io.*;
+import java.util.*;
 public class studentStatistics
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class studentStatistics
-     */
-    public studentStatistics()
+{ 
+    public static void main (String[] args){
+        
+    
+    System.out.println("This program is developed to compute statistics of students' marks in an assignment");
+    Scanner scanner = new Scanner(System.in);
+    
+   
+    while (true)
     {
-        // initialise instance variables
-        x = 0;
+        System.out.println("\nMenu");
+        System.out.println("\n1. Read students data");
+        System.out.println("\n2. Calculate the total marks for each student ");
+        System.out.println("\n3. list the students with total marks less than a the threshold");
+        System.out.println("\n4. Show five students with highest marks and lowest marks "); 
+        System.out.println("\n5. Exit the program");
+        System.out.println("\nPlease enter your choice ( 1- 5) ");
+        
+        int option = scanner.nextInt();
+        scanner.nextLine();
+        
+        switch(option){
+            case 1:
+                System.out.println("Enter the file name ");
+                break;
+            case 2:
+                calculateTotalMarks();
+                break;
+            case 3:
+                threshold();
+                break;
+            case 4:
+                showStudents();
+                break;
+            case 5:
+                System.out.println("Exit the program ");
+                break;
+            default:
+                break;
+                
+                
+                
+        }
     }
 
     /**
@@ -25,9 +59,9 @@ public class studentStatistics
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
+
+
+    
+}
 }
